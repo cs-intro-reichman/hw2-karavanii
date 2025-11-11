9f1bc8ff97833a8a961fdec9607d85d1b5b7284b
karavanii <elay.karavani@post.runi.ac.il>
2025-11-11 09:38:32 +0200

    Fixed2

--- CalcPi.java
+++ CalcPi.java
@@ -14,6 +14,6 @@ public class CalcPi {
 			denominator = denominator +2;
 		}
 		System.out.println("pi according to Java: " + Math.PI);
-		System.out.println("pi, approximated: " + (double)(sum * 4));
+		System.out.println("pi, approximated:    " + (double)(sum * 4));
 	}
 }
